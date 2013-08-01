39a6d6b5f4f0ae935c8aaf078ca58eb3dfd22c6d
St.Ack <stack@duboce.net>
2013-07-31 22:27:53 -0700

    Works against hbase-hadoop1 and hbase-hadoop2 now

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ hbase-downstreamer
 ==================
 
 Fake downstream project used figuring what is required when depending on hbase client and minicluster, etc.
+
+To build against an hbase compiled against hadoop1 (1.1.2 as of this writing), do the below:
+
+  $ mvn clean install
+
+It runs a unit test that spins up an hdfs cluster.
+
+To build against an hbase compiled against hadoop2 (2.0.5-alpha as of this writing), do the below:
+
+  $ mvn clean install -f pom.xml.hadoop2
+
+It runs the same test.
+
+Read pom.xml.hadoop2 for list of dependencies needed for test and build.
--- src/test/java/org/hbase/downstreamer/TestHBaseMiniCluster.java
+++ src/test/java/org/hbase/downstreamer/TestHBaseMiniCluster.java
@@ -1,5 +1,6 @@
 package org.hbase.downstreamer;
 
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.hbase.HBaseTestingUtility;
 import org.junit.Test;
 
@@ -10,6 +11,14 @@ public class TestHBaseMiniCluster {
   @Test
   public void testSpinUpMiniHBaseCluster() throws Exception {
     HBaseTestingUtility htu = new HBaseTestingUtility();
+    /*
+    System.out.println(FileSystem.FS_DEFAULT_NAME_KEY);
+    System.out.println(htu.getConfiguration().get(FileSystem.FS_DEFAULT_NAME_KEY));
+    htu.getConfiguration().set(FileSystem.FS_DEFAULT_NAME_KEY, "file://tmp/");
+    System.out.println(htu.getConfiguration().get(FileSystem.FS_DEFAULT_NAME_KEY));
+    System.out.println(FileSystem.class.getClassLoader());
+    FileSystem fs = FileSystem.get(htu.getConfiguration());
+    System.out.println(fs.toString());*/
     try {
       htu.startMiniCluster();
       htu.getMetaTableRows();
